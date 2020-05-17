86b68f120d6630bde8cceb6064522793a0a3747c
Gou <nakamura462645@gmail.com>
2020-05-17 11:52:48 +0900

    added defun myapp--parse

--- myapp.el
+++ myapp.el
@@ -1,19 +1,36 @@
-(defcustom myapp-list (a-alist) "list")
+;; parse repository
 
-(defun myapp--add-to-list (&rest kvs)
-  (loop for (k v) in (-partition 2 kvs)
-	do
-	(setq myapp-list (a-assoc myapp-list k v)))
-  myapp-list)
+(setq myapp--repository-file "myapp-repository")
 
-(defun myapp--initialize-list ()
-  (setq myapp-list (a-alist)))
+(defun myapp--get-repository-content ()
+  (with-temp-buffer
+    (insert-file-contents
+     (concat default-directory myapp--repository-file))
+    (buffer-string)))
+
+(defun myapp--parse
+    ()
+  "Parses repository file and returns alist."
+  (mylet [re (rx bow (group-n 1  (+ alnum) eow )
+		 (+ (or "\n" (+ blank)))
+		 (group-n 2 (+ anything) ".jar"))
+	     s (myapp--get-repository-content)
+	     ret (a-alist)]
+	 (loop for (_ k v) in (s-match-strings-all re s)
+	       do (setq ret (a-assoc ret k v)))
+	 ret))
+
+;; demo
 
 (defun myapp--run-jar (path)
   (shell-command (concat "java -jar " path)))
 
 (defun myapp-run ()
   (interactive)
-  (mylet [k (ido-completing-read "select app: "
-				 (a-keys myapp-list))]
-	 (myapp--run-jar (a-get myapp-list k))))
+  (mylet [coll (myapp--parse)
+	       k (ido-completing-read "select app: "
+				      (a-keys coll))]
+	 (myapp--run-jar (a-get coll k))))
+
+;; add new-file 
+(a-get myapp-list "pdfcrop")
